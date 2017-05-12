ece54688e1f04fcb162e5fbc83725b52f5533dc2
ricardo <ricardo.izasiga@gmail.com>
2017-05-12 12:11:35 -0500

    config_initializer_and_time_zone

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,13 @@ module Dribblers
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    
+    config.time_zone = 'America/Lima'
+    config.active_record.default_timezone = :utc
+
+    config.autoload_paths << Rails.root.join('lib')
+    config.autoload_paths += Dir[File.join(Rails.root, "lib", "extension", "*.rb")].each {|l| require l }
+
     config.api_only = true
   end
 end
