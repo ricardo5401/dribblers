434f4ae2f15b41d1e8d484b2923a467ede0d1414
Richard Julca <ricardo.izasiga@gmail.com>
2017-05-17 14:36:38 +0000

    stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  Tip.create(training_plan_id: 1, name: "", description: "")
+  
+  TrainingActivity.create(training_plan_id: 1, shoot_count: 10, with_time: false, expected_time: DateTime.now)
+  
+  TrainingPlan.create(name: "", shoot_type: "Long")
+  
+  Activity.create(user_id: 2, training_activity_id: 1, failed: 0, asserted: 0, startted_at: DateTime.now)
